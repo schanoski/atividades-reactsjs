756ef0eb029e47746697722f129a2f579063f86e
William Schanoski <schanoski97@gmail.com>
2020-10-15 23:03:02 -0300

    criacao do projeto cadastro-produtos

--- cadastro-produtos/src/index.js
+++ cadastro-produtos/src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App nome="William Schanoski" idade={22} />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
